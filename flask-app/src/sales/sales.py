d1c60e865ed817ee356bc9a4f4a1d2679955f586
Peyton Sullivan <sullivan.pey@northeastern.edu>
2023-04-14 21:07:29 -0400

    added example route and tests

--- flask-app/src/sales/sales.py
+++ flask-app/src/sales/sales.py
@@ -4,4 +4,21 @@ from src import db
 
 sales = Blueprint('sales', __name__)
 
-# @sales.route('')
+@sales.route('/advertisements', methods=['GET'])
+def get_ads():
+    query = '''
+            SELECT *
+            FROM Advertisements
+            '''
+    
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+
+    json_data = []
+    column_headers = [x[0] for x in cursor.description]
+    data = cursor.fetchall()
+
+    for row in data:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
